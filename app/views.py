ed5390a5a7567656c5a34498dcb7a80b47cd974a
MathSouza13 <36547617+MathSouza13@users.noreply.github.com>
2018-04-27 22:09:42 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -73,15 +73,5 @@ def cadastro_vestibulares(request):
             'vestibulares': Vestibular.objects.all(),
             'year':datetime.now().year,
         })
-def cadastro_candidatos(request):
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/cadastro_candidatos.html',
-        context_instance = RequestContext(request,
-        {
-            'title':'Cadastro de Candidatos',
-            'candidatos': Candidato.objects.all(),
-        })
     )
 
